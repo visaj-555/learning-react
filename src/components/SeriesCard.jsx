39ab2dee3a4099312e91306068c4e5e3ebaef084
visaj-cilans <visaj.cilans@gmail.com>
2026-04-06 19:50:55 +0530

    Enhanced UI : Added CSS Modules

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -3,16 +3,17 @@ export const SeriesCard = ({ data }) => {
 
   return (
     <li className="card">
-      <div>
-        <img src={img_url} alt={name} width="30%" />
+      <img src={img_url} alt={name} />
 
-        <div className="card-content">
-          <h2>Show Name: {name}</h2>
-          <h3>Genre: {genre.join(", ")}</h3>
-          <h4>Rating: {rating}</h4>
-          <p>Story: {description}</p>
-          <button className="btn">Watch Now</button>
-        </div>
+      <div className="card-content">
+        <h2>{name}</h2>
+        <h3>{genre.join(", ")}</h3>
+
+        <p className="rating">⭐ {rating}</p>
+
+        <p className="description">{description}</p>
+
+        <button className="btn">Watch Now</button>
       </div>
     </li>
   );
