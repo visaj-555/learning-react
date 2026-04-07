0b969188f2652d18845751643a50764e545e4f00
visaj-cilans <visaj.cilans@gmail.com>
2026-04-07 22:01:40 +0530

    Solved Tailwind connection issue

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -5,12 +5,12 @@ export const SeriesCard = ({ data }) => {
     <li className="card">
       <img src={img_url} alt={name} />
 
-      <div className="card-content">
+      <div className="flex flex-col gap-2">
         <h2>{name}</h2>
         <h3>{genre.join(", ")}</h3>
         <p className="rating">⭐ {rating}</p>
         {/* <p className="description">{description}</p> */}
-        <p className="text-3xl font-bold underline">{description}</p>
+        <p className="text-1xl font-bold underline">{description}</p>
         <button className="btn">Watch Now</button>
       </div>
     </li>
